d98dafd6ec200997e20f740441aff4b7539cd925
BASILIO CALIXTO <basiliocalixto@MacBook-Pro-de-BASILIO.local>
2022-08-29 16:48:10 -0500

    1.8: unicafe, paso 3

--- part1/src/index.js
+++ part1/src/index.js
@@ -12,9 +12,27 @@ const Button = ({onClick, text}) => (
 const Item = ({text, data}) => (
   <p>{text} {data}</p>
 )
+
 const ItemPercentaje = ({text, data}) => (
   <p>{text} {data*100} %</p>
 )
+
+const Statistics = (props) => {
+  const { title, good, neutral, bad, total, averange, positive } = props.data
+
+  return (
+    <>
+      <Title title={title} />
+      <Item text="good" data={good} />
+      <Item text="neutral" data={neutral} />
+      <Item text="bad" data={bad} />
+
+      <Item text="total" data={total} />
+      <Item text="averange" data={averange} />
+      <ItemPercentaje text="positive" data={positive} />
+    </>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -24,20 +42,24 @@ const App = () => {
   const total = good + neutral + bad
   const averange = (good - bad) / total
   const positive = good / total
+
+  const statistincsObj = {
+    title: "statistics",
+    good,
+    neutral,
+    bad,
+    total,
+    averange,
+    positive
+  }
+
   return (
     <div>
       <Title title="give feedback" />
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-      <Title title="statistics" />
-      <Item text={"good"} data={good} />
-      <Item text={"neutral"} data={neutral} />
-      <Item text={"bad"} data={bad} />
-
-      <Item text={"total"} data={total} />
-      <Item text={"averange"} data={averange} />
-      <ItemPercentaje text={"positive"} data={positive} />
+      <Statistics data={statistincsObj} />
     </div>
   )
 }
