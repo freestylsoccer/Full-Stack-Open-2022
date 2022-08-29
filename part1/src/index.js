c0ca863bf590f3045293e30fecd5827585124362
BASILIO CALIXTO <basiliocalixto@MacBook-Pro-de-BASILIO.local>
2022-08-29 17:35:16 -0500

    1.12*: anécdotes, paso 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,95 +1,33 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Title = ({title}) => (
-  <h1>{title}</h1>
-)
-
-const Button = ({onClick, text}) => (
-  <button onClick={onClick}>{text}</button>
-)
-
-const Statistic = ({text, value}) => (
-  <p>{text} {value}</p>
-)
-
-const StatisticPercentaje = ({text, value}) => (
-  <p>{text} {value*100} %</p>
-)
-
-const Statistics = (props) => {
-  const { good, neutral, bad, total, averange, positive } = props.data
-
-  if (total === 0) {
-    return <p>No feedback given</p>
-  }
-
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>{"good"}</td>
-          <td>{good}</td>
-        </tr>
-        <tr>
-          <td>{"neutral"}</td>
-          <td>{neutral}</td>
-        </tr>
-        <tr>
-          <td>{"bad"}</td>
-          <td>{bad}</td>
-        </tr>
-        <tr>
-          <td>{"all"}</td>
-          <td>{total}</td>
-        </tr>
-        <tr>
-          <td>{"averange"}</td>
-          <td>{averange}</td>
-        </tr>
-        <tr>
-          <td>{"positive"}</td>
-          <td>{positive * 100}%</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad
-  const averange = (good - bad) / total
-  const positive = good / total
-
-  const statistincsObj = {
-    good,
-    neutral,
-    bad,
-    total,
-    averange,
-    positive
-  }
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  
+  const getRandomAnecdote = () =>
+    setSelected(Math.floor(Math.random() * (5 - 0 + 1)) + 0)
 
   return (
     <div>
-      <Title title="give feedback" />
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
-      <Title title="statistics" />
-      <Statistics data={statistincsObj} />
+      <p>{props.anecdotes[selected]}</p>
+      <button onClick={() => getRandomAnecdote()}>get anecdote</button>
     </div>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App anecdotes={anecdotes}/>
   </React.StrictMode>
 );
 
