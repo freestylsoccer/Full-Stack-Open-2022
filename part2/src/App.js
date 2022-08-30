07d4cc9dfbfabbc599cf66dc78bc3aa228c552c3
BASILIO CALIXTO <basiliocalixto@MacBook-Pro-de-BASILIO.local>
2022-08-30 15:29:58 -0500

    2.11: La guía telefónica Paso6

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
 
   const [ nameFilter, setNameFilter ] = useState('')
+  useEffect(() => {
+      // console.log("use effect")
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log(response.data)
+          setPersons(response.data)
+        })
+    }, [])
   
   const addPerson = (event) => {
     event.preventDefault()
