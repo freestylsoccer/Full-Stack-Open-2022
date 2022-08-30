ffa92f8f1a157e705701d5865bf7b73bf7bc224a
BASILIO CALIXTO <basiliocalixto@MacBook-Pro-de-BASILIO.local>
2022-08-30 09:13:14 -0500

    2.3*: Información del curso, paso8

--- part2/src/Components/Course.js
+++ part2/src/Components/Course.js
@@ -5,7 +5,7 @@ const Total = ({ sum }) => <h5>total of {sum} exercises</h5>
 
 const Course = (props) => {
   const {name, parts} = props.course
-  const total = parts.reduce((p, c) => p + c.exercises, 0,)
+  const total = parts.reduce((p, c) => p + c.exercises, 0)
 
   return (
     <>
