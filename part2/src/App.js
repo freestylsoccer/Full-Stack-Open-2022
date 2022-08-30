dc3182851b574469d1b6c811477d4934a30a38e5
BASILIO CALIXTO <basiliocalixto@MacBook-Pro-de-BASILIO.local>
2022-08-30 17:48:58 -0500

    2.14*: Datos de países, paso 3

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import axios from 'axios'
 
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filterCountry, setFilterCountry] = useState('')
+  const [weather, setWeater] = useState({})
 
   useEffect(() => {
     axios
@@ -12,10 +13,35 @@ const App = () => {
         setCountries(response.data)
       })
   }, [])
-  
-  const countriesFiltered = filterCountry !== '' ? 
-    countries.filter(country => country.name.common.toLowerCase().indexOf(filterCountry.toLocaleLowerCase()) !== -1)
-    : []
+
+  const countriesFiltered = useMemo(() => {
+    const some = filterCountry !== '' ?
+      countries.filter(country => country.name.common.toLowerCase().indexOf(filterCountry.toLocaleLowerCase()) !== -1)
+      : []
+    return some
+  }, [filterCountry, countries])
+
+  useEffect(() => {
+    if(countriesFiltered.length === 1) {
+      const city = countriesFiltered[0].capital[0]
+      const params = {
+        access_key: process.env.REACT_APP_API_KEY,
+        query: city
+      }
+      axios
+      .get('http://api.weatherstack.com/current', {params})
+      .then(response => {
+          if (!response.data.error) {
+              const apiResponse = response.data;
+              setWeater(apiResponse)
+          } else {
+              console.log(`Response error: code: ${response.data.error.code}, info: ${response.data.error.info}`)
+          }
+      }).catch(error => {
+          console.error("An error occurred: ", error);
+        })
+      }
+    }, [countriesFiltered])
 
   return (
     <>
@@ -28,13 +54,18 @@ const App = () => {
           <h2>{countriesFiltered[0].name.common}</h2>
           <p>capital {countriesFiltered[0].capital}</p>
           <p>population {countriesFiltered[0].population}</p>
-          <h2>languages</h2>
+          <h3>languages</h3>
           <ul>
             {Object.entries(countriesFiltered[0].languages).map(([key, value]) =>
               <li key={key}>{value}</li>
             )}
           </ul>
           <img src={countriesFiltered[0].flags.png} width="250px" alt="flag"/>
+          
+          <h3>weather in {countriesFiltered[0].capital}</h3>
+          <p><strong>temperature:</strong> {weather?.current?.temperature} Celsius</p>
+          <img src={weather?.current?.weather_icons} alt="weather icon" />
+          <p><strong>wind:</strong> {weather?.current?.wind_speed} {weather?.current?.wind_dir}</p>
         </>
       ) : countriesFiltered.length <= 10 ? (
         <>
