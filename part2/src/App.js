69da6555acc3954efcbd34f8b3bb113f162378b0
BASILIO CALIXTO <basiliocalixto@MacBook-Pro-de-BASILIO.local>
2022-09-02 14:01:39 -0500

    2.15: Guía telefónica paso7

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,86 +1,81 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+import personService from './services/persons'
 
 const App = () => {
-  const [countries, setCountries] = useState([])
-  const [filterCountry, setFilterCountry] = useState('')
-  const [weather, setWeater] = useState({})
+  const [ persons, setPersons ] = useState([]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
+  const [ nameFilter, setNameFilter ] = useState('')
   useEffect(() => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        setCountries(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
+  
+  const addPerson = (event) => {
+    event.preventDefault()
 
-  const countriesFiltered = useMemo(() => {
-    const some = filterCountry !== '' ?
-      countries.filter(country => country.name.common.toLowerCase().indexOf(filterCountry.toLocaleLowerCase()) !== -1)
-      : []
-    return some
-  }, [filterCountry, countries])
+    if (persons.find(element => element.name === newName)) {
+      window.alert(`${newName} is already added to phonebook`)
+      return
+    }
+    
+    const person = {
+      name: newName,
+      number: newNumber
+    }
 
-  useEffect(() => {
-    if(countriesFiltered.length === 1) {
-      const city = countriesFiltered[0].capital[0]
-      const params = {
-        access_key: process.env.REACT_APP_API_KEY,
-        query: city
-      }
-      axios
-      .get('http://api.weatherstack.com/current', {params})
-      .then(response => {
-          if (!response.data.error) {
-              const apiResponse = response.data;
-              setWeater(apiResponse)
-          } else {
-              console.log(`Response error: code: ${response.data.error.code}, info: ${response.data.error.info}`)
-          }
-      }).catch(error => {
-          console.error("An error occurred: ", error);
-        })
-      }
-    }, [countriesFiltered])
+    personService
+      .create(person)
+      .then(returnedNote => {
+        setPersons(persons.concat(returnedNote))
+        setNewName('')
+        setNewNumber('')
+      })
+    
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleNameFilterChange = (event) => {
+    setNameFilter(event.target.value)
+  }
+
+  const personsToShow = nameFilter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLocaleLowerCase().indexOf(nameFilter.toLocaleLowerCase()) !== -1
+      )
 
   return (
-    <>
-      <div>
-        find countries <input value={filterCountry} onChange={(e) => setFilterCountry(e.target.value)} />
-      </div>
-
-      {countriesFiltered.length === 1 ? (
-        <>
-          <h2>{countriesFiltered[0].name.common}</h2>
-          <p>capital {countriesFiltered[0].capital}</p>
-          <p>population {countriesFiltered[0].population}</p>
-          <h3>languages</h3>
-          <ul>
-            {Object.entries(countriesFiltered[0].languages).map(([key, value]) =>
-              <li key={key}>{value}</li>
-            )}
-          </ul>
-          <img src={countriesFiltered[0].flags.png} width="250px" alt="flag"/>
-          
-          <h3>weather in {countriesFiltered[0].capital}</h3>
-          <p><strong>temperature:</strong> {weather?.current?.temperature} Celsius</p>
-          <img src={weather?.current?.weather_icons} alt="weather icon" />
-          <p><strong>wind:</strong> {weather?.current?.wind_speed} {weather?.current?.wind_dir}</p>
-        </>
-      ) : countriesFiltered.length <= 10 ? (
-        <>
-          {countriesFiltered.map(country => 
-           <div key={country.name.official}>
-              {country.name.common} 
-              <button onClick={() => setFilterCountry(country.name.common)}>show</button>
-            </div>
-          )}
-        </>
-      ) : (
-        <p>Too many matches, specify another filter</p>
-      )
-      }
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter nameFilter={nameFilter} onChange={handleNameFilterChange} />
+      <h3>add a new</h3>
+      <PersonForm
+        onSumbit={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons personsToShow={personsToShow} />
+    </div>
   )
 }
 
